cc03dc005ef0acdbd9bf5e48e3ebe92e5c1784a0
Erictrp1 <87619918+Erictrp1@users.noreply.github.com>
2022-04-01 18:47:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//HEURE
 var dateGlobale = new Date();
 
 var heure = dateGlobale.getHours();
@@ -11,20 +12,23 @@ if(minute < 10){
     minute = "0" + minute;
 }
 
-
 document.getElementById("heure").innerHTML = heure + ":" + minute;
 
 
+//CHANGEMENT DE BACKGROUND
 function changeBackground(){
-
     const images = [
         'url("images/volcan.jpg")',
         'url("images/coucher.jpg")',
+        'url("images/plage.jpg")',
     ]
 
     const section = document.querySelector('section')
     const bg = images[Math.floor(Math.random() * images.length)];
     section.style.background= bg;
+    section.style.backgroundSize= "cover";
+    section.style.backgroundPosition= "center";
+    section.style.transition = "all.5s";
 }
 
 setInterval(changeBackground, 5000)
